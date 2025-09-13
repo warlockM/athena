b0f1fee90de91883e19f6e63548f94153b5a7bfb
mayank-doodleblue <mayank.r@doodleblue.com>
2025-09-13 09:30:47 +0530

    Updated cors issue fixes and rest framework fixes

--- scrape/settings.py
+++ scrape/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'scrape',
     'tracking',
     'api',
@@ -58,7 +59,17 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
 
 ROOT_URLCONF = 'scrape.urls'
 
